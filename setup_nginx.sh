eb4addad13faa0afe1c01c7e8548a386ed2efada
Cristian Reynaga <cristianreynaga@gmail.com>
2026-09-25 10:47:50 -0300

    fix: usar location ^~ /bici/ en Nginx para prioridad sobre regex de estaticos

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -15,7 +15,7 @@ server {
                 return 301 /bici/;
         }
 
-        location /bici/ {
+        location ^~ /bici/ {
                 proxy_pass http://127.0.0.1:8095/;
                 proxy_http_version 1.1;
                 proxy_set_header Upgrade $http_upgrade;
@@ -50,7 +50,7 @@ server {
         return 301 /bici/;
     }
 
-    location /bici/ {
+    location ^~ /bici/ {
         proxy_pass http://127.0.0.1:8095/;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
